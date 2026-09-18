49718a6bd0bbfa0d4c04b9a1647332a8912f0163
Farhan-Khan7 <fk5518744@gmail.com>
2026-09-18 23:06:40 +0530

    feat: add user and post schemas

---
+++ Backend/src/models/Project.models.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import monogoose, { Schema } from "mongoose";
+
+const ProjectSchema = new Schema(
+    {
+        title: {
+            type: String,
+            require: true,
+            unique: true,
+        },
+        description: {
+            type: String,
+            require: true,
+        },
+        createdBy: {
+            type: Schema.Types.ObjectId,
+            ref: "userModel",
+        },
+        createdOn: {
+            type: Date,
+            require: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+
+const projectModel = Schema.model("Projects" , ProjectSchema)
+
+
+export default projectModel
