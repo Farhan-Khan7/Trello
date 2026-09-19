702c9e58356ac821d53afcd3821265e72bea678d
Farhan-Khan7 <fk5518744@gmail.com>
2026-09-19 06:57:55 +0530

    feat : add mailtrap for verify profile

--- Backend/src/controllers/auth.controllers.js
+++ Backend/src/controllers/auth.controllers.js
@@ -1,14 +1,14 @@
 import config from "../dbConnect/config.js";
 import userModel from "../models/User.models.js";
 import ApiError from "../utils/api-errors.js";
-import nodemailer from "nodemailer"
+import nodemailer from "nodemailer";
 // import ApiResponse from "../utils/api-response.js"
 
 const registerUser = async (req, res) => {
     const { userName, email, password } = req.body;
-    console.log("enter ho gaya controllers me !")
+    console.log("enter ho gaya controllers me !");
 
-    console.log(userName, email, password, "\n")
+    console.log(userName, email, password, "\n");
 
     if (!userName || !email || !password) {
         throw new ApiError(404, "userName Email & Password are required!");
@@ -18,13 +18,13 @@ const registerUser = async (req, res) => {
         $or: [{ email, userName }],
     });
 
-    console.log(existingUser, "\n")
+    console.log(existingUser, "\n");
 
     if (existingUser) {
         return res.status(400).json({
             success: false,
-            message: "User is exist with this email !"
-        })
+            message: "User is exist with this email !",
+        });
     }
 
     const user = await userModel.create({
@@ -33,19 +33,16 @@ const registerUser = async (req, res) => {
         password,
     });
 
-    console.log(user, "\n")
+    console.log(user, "\n");
 
     const { verificationToken, hashedToken, expiresAt } = await user.generateEmailVerificationToken();
 
-    user.emailVerficationToken = hashedToken
-    user.emailVerficationExpires = expiresAt
+    user.emailVerficationToken = hashedToken;
+    user.emailVerficationExpires = expiresAt;
 
+    console.log(user.emailVerficationToken, "\n");
 
-    console.log(user.emailVerficationToken, "\n")
-
-
-    await user.save({ validateBeforeSave: false })
-
+    await user.save({ validateBeforeSave: false });
 
     const transporter = nodemailer.createTransport({
         host: config.MAILTRAP_HOST,
@@ -53,35 +50,29 @@ const registerUser = async (req, res) => {
         secure: false,
         auth: {
             user: config.MAILTRAP_USER,
-            pass: config.MAILTRAP_PASS
-        }
-    })
+            pass: config.MAILTRAP_PASS,
+        },
+    });
 
     const mailOptions = {
         from: config.MAILTRAP_SENDER,
         to: user.email,
         subject: `Please Verfiy your email `,
-        text: `${config.BASE_URL}//api/v1/auth/profileverfiy${verificationToken}`
-    }
+        text: `${config.BASE_URL}//api/v1/auth/profileverfiy${verificationToken}`,
+    };
 
     transporter.sendMail(mailOptions, (err, info) => {
         if (err) {
-            return console.log(`Email not sent to ${user.userName}`)
+            return console.log(`Email not sent to ${user.userName}`);
         } else {
-            return console.log(`Email sent to ${user.userName} \n ${info.messageId}`)
+            return console.log(`Email sent to ${user.userName} \n ${info.messageId}`);
         }
-    })
-
-
+    });
 
     res.status(201).json({
         success: true,
-        message: "User Registered Successfully !"
-    })
-
+        message: "User Registered Successfully !",
+    });
 };
 
-
-export {
-    registerUser
-}
+export { registerUser };
