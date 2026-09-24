06687c21fc37de5f0a54bd11f15c2f59f5ed4cfc
Farhan-Khan7 <fk5518744@gmail.com>
2026-09-25 03:56:43 +0530

    feat(project) : Done projectNote API's

--- Backend/src/controllers/projectNotes.controllers.js
+++ Backend/src/controllers/projectNotes.controllers.js
@@ -1,114 +1,164 @@
 import projectNotesModel from "../models/ProjectNote.models.js";
-import projectModels from "../models/Project.models.js"
+import projectModels from "../models/Project.models.js";
 import userModel from "../models/User.models.js";
 
-
-
-
 // create projects Note API Completed
-const createProjectNote = async (req , res) => {
-    const { notes , projectReference } = req.body
-    const id = req.user.id
-    if(!notes){
+const createProjectNote = async (req, res) => {
+    const { notes, projectReference } = req.body;
+    const id = req.user.id;
+    if (!notes) {
         return res.status(404).json({
-            success : false,
-            message : "notes is reqired!"
-        })
+            success: false,
+            message: "notes is reqired!",
+        });
     }
 
-    const projectId = await projectModels.findOne({title : projectReference})
+    const projectId = await projectModels.findOne({ title: projectReference });
 
-    if(!projectId){
+    if (!projectId) {
         return res.status(404).json({
-            success : false,
-            message : "Project ID not found!"
-        })
+            success: false,
+            message: "Project ID not found!",
+        });
     }
 
-    const user = await userModel.findById(id)
+    const user = await userModel.findById(id);
 
-    if(!user){
+    if (!user) {
         return res.status(404).json({
-            success : false,
-            message : "user not found!"
-        })
+            success: false,
+            message: "user not found!",
+        });
     }
 
     const projectNote = await projectNotesModel.create({
         notes,
-        projectReference : projectId._id,
-        createdBy : user._id
-    })
+        projectReference: projectId._id,
+        createdBy: user._id,
+    });
+
+    res.status(200).json({
+        success: true,
+        message: "Projects Notes Created Successfully!",
+    });
+};
+
+// Get All Project Notes By Projects Completed
+const getAllProjectNotesByProjects = async (req, res) => {
+    const id = req.params.id;
+
+    if (!id) {
+        return res.status(404).json({
+            success: false,
+            message: "Project Notes ID not found!",
+        });
+    }
+
+    const projectNotes = await projectNotesModel.find({ projectReference: id });
+
+    if (!projectNotes) {
+        return res.status(404).json({
+            success: false,
+            message: "projects note not found!",
+        });
+    }
+
+    res.status(200).json({
+        success: true,
+        message: "Project Notes Fetched Successfully!",
+        data : {
+            projectNotes
+        }
+    });
+};
+
+// Update Projects Notes API Completed
+const updateProjectNotes = async (req , res) => {
+    const { notes } = req.body
+    const id = req.params.id
+
+    if(!id){
+        return res.status(404).json({
+            success : false,
+            message : "notes id not found"
+        })
+    }
+
+    const updateNote = await projectNotesModel.findByIdAndUpdate(
+        id,
+        {
+            notes
+        },
+        {
+            returnDocument : "after"
+        }
+    )
 
 
     res.status(200).json({
         success : true,
-        message : "Projects Notes Created Successfully!"
+        message : "Note Update Successfully!",
+        data : {
+            updateNote
+        }
     })
+
 
 }
 
 // completed Projects Notet API Done
-const completedProjectNote = async (req , res) => {
-    const id = req.params.id
+const completedProjectNote = async (req, res) => {
+    const id = req.params.id;
 
-    if(!id){
+    if (!id) {
         return res.status(404).json({
-            success : false,
-            message : "Note ID not found!"
-        })
+            success: false,
+            message: "Note ID not found!",
+        });
     }
 
-    const completedProjectNotes = await projectNotesModel.findById(id)
+    const completedProjectNotes = await projectNotesModel.findById(id);
 
-    if(!completedProjectNotes){
+    if (!completedProjectNotes) {
         return res.status(404).json({
-            success : false,
-            message : "Project Nots does not found!"
-        })
+            success: false,
+            message: "Project Nots does not found!",
+        });
     }
 
-    completedProjectNotes.isSolved = true
+    completedProjectNotes.isSolved = true;
 
-    await completedProjectNotes.save()
+    await completedProjectNotes.save();
 
     res.status(200).json({
-        success : true,
-        message : "Porject Completed Successfully!",
-        data : {
-            completedProjectNotes
-        }
-
-    })
-}
+        success: true,
+        message: "Porject Completed Successfully!",
+        data: {
+            completedProjectNotes,
+        },
+    });
+};
 
 // delete projects Note API Completed
-const deleteProjectNote = async (req , res) => {
-    const id = req.params.id
+const deleteProjectNote = async (req, res) => {
+    const id = req.params.id;
 
-    if(!id){
+    if (!id) {
         return res.status(404).json({
-            success : false,
-            message : "Note ID not found!"
-        })
+            success: false,
+            message: "Note ID not found!",
+        });
     }
 
-    const deletedProjectNote = await projectNotesModel.findByIdAndDelete(id)
+    const deletedProjectNote = await projectNotesModel.findByIdAndDelete(id);
 
     res.status(200).json({
-        success : true,
-        message : "Note Deleted Successfully!",
-        data : {
-            deletedProjectNote
-        }
-    })
+        success: true,
+        message: "Note Deleted Successfully!",
+        data: {
+            deletedProjectNote,
+        },
+    });
+};
 
-
-}
-
-
-export {
-    createProjectNote,
-    deleteProjectNote,
-    completedProjectNote
-}
+export { createProjectNote, deleteProjectNote, completedProjectNote, getAllProjectNotesByProjects, updateProjectNotes };
--- Backend/src/routes/projectNotes.routes.js
+++ Backend/src/routes/projectNotes.routes.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
-import { createProjectNote , deleteProjectNote , completedProjectNote } from "../controllers/projectNotes.controllers.js";
+import { createProjectNote, deleteProjectNote, completedProjectNote, getAllProjectNotesByProjects, updateProjectNotes } from "../controllers/projectNotes.controllers.js";
 import { isLogged } from "../middlewares/authLogged.middleware.js"
 
 const projectNotesRouter = Router()
 
 
-projectNotesRouter.route("/create-project-note").post(isLogged , createProjectNote)
-projectNotesRouter.route("/completed-project/:id").get(completedProjectNote)
-projectNotesRouter.route("/delete-project/:id").get(deleteProjectNote)
+projectNotesRouter.route("/create-project-note").post(isLogged, createProjectNote)
+projectNotesRouter.route("/completed-project-note/:id").get(completedProjectNote)
+projectNotesRouter.route("/get-all-notes-selected-projects/:id").get(getAllProjectNotesByProjects)
+projectNotesRouter.route("/update-project-note/:id").post(updateProjectNotes)
+projectNotesRouter.route("/delete-project-note/:id").get(deleteProjectNote)
 
 
 
